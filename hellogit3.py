ebd105c3d03bec5afa192b08f9049fd5ff1d679a
TheJoe <bjeferssonvinicio2005@gmail.com>
2024-02-14 15:06:45 -0500

    Solucion de conflicto

--- hellogit3.py
+++ hellogit3.py
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
-print("Soy el tercer archivo Modificado por Login V3");
-=======
-print("Soy el tercer archivo Modificado por Login v2");
->>>>>>> main
 
+print("Soy el tercer archivo Modificado por Login V3");

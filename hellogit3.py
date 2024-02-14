bf374ced4f0a13dd3c24167e650d0ceac145627d
TheJoe <bjeferssonvinicio2005@gmail.com>
2024-02-14 14:47:53 -0500

    Modificacion prueba conflictos

--- hellogit3.py
+++ hellogit3.py
@@ -1,10 +1,2 @@
-print("Soy el tercer archivo Modificado por Login");
+print("Soy el tercer archivo Modificado por Login V3");
 
-print ("Modificacion en la rama de login");
-
-
-print ("Soy una nueva modificacion");
-
-a=10;
-
-b:10;
